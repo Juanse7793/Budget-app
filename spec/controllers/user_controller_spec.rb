1c765427989a619843d4b73b9cc8834e556f6013
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-08-04 14:52:41 -0500

    linters

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 require 'spec_helper'
 
 RSpec.describe User, type: :request do
-    before(:each) do
-        @user = User.create(name: 'test', email: 'test@test.com', password: 'testtest')
+  before(:each) do
+    @user = User.create(name: 'test', email: 'test@test.com', password: 'testtest')
+  end
+  describe 'GET /users' do
+    it 'returns http success' do
+      get '/users'
+      expect(response).to have_http_status(:success)
     end
-    describe 'GET /users' do
-        it 'returns http success' do
-            get '/users'
-            expect(response).to have_http_status(:success)
-        end
-    end
+  end
 end
